90c813a9bc4170a1fc3b52452f3955490ecf08c7
acej0k3r <ace.rav3n@gmail.com>
2024-10-06 02:05:36 -0400

    just some added test logic

--- src/features/roshoon/Roshoon.jsx
+++ src/features/roshoon/Roshoon.jsx
@@ -4,13 +4,23 @@ import { useSelector } from 'react-redux';
 const Roshoon = () => {
   const roshoon = useSelector((state) => state.roshoon);
   console.log('search object =>', roshoon);
+  
+  function findMatchedChefs(){
+    return roshoon.filteredChefs?.map((chef) => {
+      const searchRegex = new RegExp(roshoon.search, 'i');
+      return (
+        <li
+          key={chef.id}
+        >{`${chef.name} located at ${chef.deliveryAreas.find((area) => searchRegex.test(area)) || 'unidentified area'}`}</li>
+      );
+    });
+  }
+  
   return (
     <div>
       <h1>Available Chefs {roshoon.search && `for "${roshoon?.search}"`}</h1>
       <ul>
-        {roshoon.filteredChefs?.map((chef) => (
-          <li key={chef.id}>{chef.name}</li>
-        ))}
+        {findMatchedChefs()}
       </ul>
     </div>
   );
