a6ff0ed9219364f4b630971edfeccf3bd65ee769
anishazahan <anishazahan13@gmail.com>
2024-12-07 23:34:09 +0600

    learn more section design

--- src/features/roshoon/Roshoon.jsx
+++ src/features/roshoon/Roshoon.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Carousel from 'src/components/Carousel';
+import LearnMore from 'src/components/LandingComponents/LearnMore';
 import { slides } from 'src/data/Slides';
 
 const Roshoon = () => {
   return (
     <div className="w-full flex flex-col">
       <Carousel slides={slides} autoSlide={true} slideDuration={5} />
+      <LearnMore />
     </div>
   );
 };
