58360c245cbec6c3feba85867405af36eceb4000
fardin785 <fardin785@gmail.com>
2025-03-03 00:05:48 +0600

    server url added

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.roshoonreact.app',
   appName: 'roshoon-react',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    url: 'https://roshoon.com',
+    cleartext: true,
+  },
 };
 
 export default config;
